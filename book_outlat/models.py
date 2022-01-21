bb422b9ab66ec3d64cb53c34e219bd89e7bc3299
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-21 20:53:03 +0600

    adding models.py and html files  with database

--- book_outlat/models.py
+++ book_outlat/models.py
@@ -1,7 +1,9 @@
 # from turtle import title
+from ast import arg
+from django.urls import reverse
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -9,9 +11,17 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1),
-                    MaxValueValidator(5)])
+                     MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) # Harry Potter 1 => harry-potter-1
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args={self.slug})
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
