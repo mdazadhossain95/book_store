10beab7b489ebaabf7b546c3f1a70a9971f19722
Md Azad Hossain Tutul <mdazadhossain95@gmail.com>
2022-01-22 17:34:52 +0600

    update views.py and index.html

--- book_outlat/views.py
+++ book_outlat/views.py
@@ -1,15 +1,24 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
 
 from .models import Book
+
 
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
+
     return render(request, "book_outlat/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
+
     })
 
 
